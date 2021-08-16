73d3041c66cf19577955f6d01eb6a7088579bd94
AnjarMaulana <rajnaanaluam36@gmail.com>
2021-08-16 19:45:00 +0700

    mulai project

---
+++ src/test/java/org/javajarvis/SistemCuti_UASJava/SistemCutiUasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javajarvis.SistemCuti_UASJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemCutiUasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
