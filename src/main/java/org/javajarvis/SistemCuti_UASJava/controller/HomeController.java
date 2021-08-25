f2958f481513683493c083644fdb0348f882a22d
AnjarMaulana <rajnaanaluam36@gmail.com>
2021-08-26 01:51:53 +0700

    FE gambaran

---
+++ src/main/java/org/javajarvis/SistemCuti_UASJava/controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.javajarvis.SistemCuti_UASJava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+class HomeController {
+
+    @GetMapping()
+    String index() {
+        return "index";
+    }
+}
