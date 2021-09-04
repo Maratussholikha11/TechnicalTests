7a113bd43aaf11b99cb7fe88e601feba57a858f1
KangAde <aderadiarahmat08@gmail.com>
2021-09-04 09:49:49 +0700

    fixing error

--- src/main/java/org/javajarvis/SistemCuti_UASJava/dto/PengajuanCutiDto.java
+++ src/main/java/org/javajarvis/SistemCuti_UASJava/dto/PengajuanCutiDto.java
@@ -13,4 +13,76 @@ public class PengajuanCutiDto {
     private String noTelp;
     private String keterangan;
     private int lamaCuti;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getStatusCutiId() {
+        return statusCutiId;
+    }
+
+    public void setStatusCutiId(int statusCutiId) {
+        this.statusCutiId = statusCutiId;
+    }
+
+    public int getPenggantiId() {
+        return penggantiId;
+    }
+
+    public void setPenggantiId(int penggantiId) {
+        this.penggantiId = penggantiId;
+    }
+
+    public int getHrdId() {
+        return hrdId;
+    }
+
+    public void setHrdId(int hrdId) {
+        this.hrdId = hrdId;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public int getLamaCuti() {
+        return lamaCuti;
+    }
+
+    public void setLamaCuti(int lamaCuti) {
+        this.lamaCuti = lamaCuti;
+    }
 }
--- src/main/java/org/javajarvis/SistemCuti_UASJava/model/PengajuanCuti.java
+++ src/main/java/org/javajarvis/SistemCuti_UASJava/model/PengajuanCuti.java
@@ -44,4 +44,75 @@ public class PengajuanCuti {
     @Column(name = "lama_cuti")
     private Integer lamaCuti;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public StatusCuti getStatusCuti() {
+        return statusCuti;
+    }
+
+    public void setStatusCuti(StatusCuti statusCuti) {
+        this.statusCuti = statusCuti;
+    }
+
+    public Employee getPengganti() {
+        return pengganti;
+    }
+
+    public void setPengganti(Employee pengganti) {
+        this.pengganti = pengganti;
+    }
+
+    public Employee getHrd() {
+        return hrd;
+    }
+
+    public void setHrd(Employee hrd) {
+        this.hrd = hrd;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public Integer getLamaCuti() {
+        return lamaCuti;
+    }
+
+    public void setLamaCuti(Integer lamaCuti) {
+        this.lamaCuti = lamaCuti;
+    }
 }
